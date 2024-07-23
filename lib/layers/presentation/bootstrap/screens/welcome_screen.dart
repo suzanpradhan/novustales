aba01cd2b79e567eafe55b4827ef54a217e5b2dc
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2024-07-23 20:09:18 +0545

    feat: welcome screen ui

--- lib/layers/presentation/bootstrap/screens/welcome_screen.dart
+++ lib/layers/presentation/bootstrap/screens/welcome_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:storyv2/core/constants/app_colors.dart';
+import 'package:storyv2/core/constants/ui_constants.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -10,9 +12,69 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Welcome Screen"),
+    return Scaffold(
+      backgroundColor: AppColors.purpleAccent,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(UIConstants.maxPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  SizedBox(
+                    height: 42,
+                  ),
+                  Text(
+                    "Meet,\nShare,\nand\nExplore",
+                    style: TextStyle(
+                        fontFamily: "RalewayBlack",
+                        color: AppColors.white,
+                        height: 1.2,
+                        fontSize: 62),
+                  ),
+                  SizedBox(
+                    height: UIConstants.padding,
+                  ),
+                  Text(
+                    "Explore popular spots, hidden gems, and upcoming events. Find your next adventure now!",
+                    style: TextStyle(
+                        fontFamily: "SatoshiRegular",
+                        fontSize: 16,
+                        color: AppColors.white),
+                  ),
+                ],
+              ),
+              Container(
+                height: 50,
+                padding: EdgeInsets.symmetric(horizontal: UIConstants.padding),
+                decoration: BoxDecoration(
+                    color: AppColors.black,
+                    borderRadius:
+                        BorderRadius.circular(UIConstants.borderRadius)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Get Started",
+                      style: TextStyle(
+                          fontFamily: "Satoshi",
+                          color: AppColors.white,
+                          fontSize: 16),
+                    ),
+                    Icon(
+                      Icons.arrow_right_alt_sharp,
+                      color: AppColors.white,
+                      size: 32,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
