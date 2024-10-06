a9f1cc976059a6e93e0f5d4c1a6c12e9d8562f05
Niwesh Shrestha <niweshs@gmail.com>
2024-10-06 21:38:55 +0545

    feat: todays change

--- lib/core/presentation/widgets/count_info_card_widget.dart
+++ lib/core/presentation/widgets/count_info_card_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:storyv2/core/constants/ui_constants.dart';
 
+import '../../constants/app_colors.dart';
 import '../ui/spacer.dart';
 
 class CountInfoCardWidget extends StatelessWidget {
@@ -8,16 +10,41 @@ class CountInfoCardWidget extends StatelessWidget {
     required this.text1,
     required this.text2,
   });
-  final Text text1;
-  final Text text2;
+  final String text1;
+  final String text2;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        flex: 1,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(UIConstants.minBorderRadius),
+            color: AppColors.purpleAccent.withOpacity(0.1)),
+        width: double.maxFinite,
+        height: 75,
+        padding: EdgeInsets.all(UIConstants.minPadding),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [text1, Gapper.v2xmGap(), text2],
-        ));
+          children: [
+            Text(
+              text1,
+              style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                    fontFamily: "UberBold",
+                    color: AppColors.black,
+                  ),
+            ),
+            Gapper.v2xmGap(),
+            Text(
+              text2,
+              style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                    fontFamily: "UberMedium",
+                    color: AppColors.black.withOpacity(.6),
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/layers/presentation/tales/widgets/ShiftableImageHolderWidget.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/constants/app_colors.dart';
+import '../../../../core/constants/assets.dart';
+
+class ShiftableimageholderWidget extends StatefulWidget {
+  final double imageSize;
+  final bool showCount;
+  final int? count;
+  final MainAxisAlignment? mainAxisAlignment;
+  final double? areaHeight;
+  final Color? backgroundColor;
+  final Text? title;
+  final Color? ringColor;
+  final Color? dropShadow;
+  const ShiftableimageholderWidget({
+    super.key,
+    required this.imageSize,
+    this.count,
+    this.showCount = true,
+    this.mainAxisAlignment,
+    this.areaHeight,
+    this.backgroundColor,
+    this.title,
+    this.ringColor,
+    this.dropShadow,
+  });
+
+  @override
+  State<ShiftableimageholderWidget> createState() =>
+      _ShiftableimageholderWidgetState();
+}
+
+class _ShiftableimageholderWidgetState
+    extends State<ShiftableimageholderWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: widget.backgroundColor ?? Colors.transparent,
+        height: widget.areaHeight ?? 180,
+        child: Column(
+          mainAxisAlignment: widget.mainAxisAlignment ?? MainAxisAlignment.end,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  height: widget.imageSize + 15,
+                ),
+                Container(
+                  width: widget.imageSize,
+                  height: widget.imageSize,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: widget.ringColor ?? AppColors.purpleAccent,
+                      width: 2,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: widget.dropShadow ??
+                            AppColors.purpleAccent.withOpacity(0.7),
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                ),
+                // Circular image inside the border
+                Container(
+                  width: widget.imageSize -
+                      5, // Image size (should be slightly smaller than the outer border)
+                  height: widget.imageSize - 5,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  child: Image.asset(Assets.test, fit: BoxFit.cover),
+                ),
+                if (widget.showCount)
+                  Positioned(
+                    bottom: 0,
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: widget.ringColor ?? AppColors.purpleAccent,
+                      ),
+                      child: Center(
+                        child: Text(
+                          "${widget.count ?? 0}",
+                          style:
+                              Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                    fontFamily: "UberBold",
+                                    color: AppColors.white,
+                                  ),
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            widget.title ?? SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
