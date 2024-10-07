5326bee8ef1cdda533f90a09286bb2f88cd103f8
Niwesh Shrestha <niweshs@gmail.com>
2024-10-07 14:06:47 +0545

    feat: new feature

--- lib/layers/presentation/tales/widgets/menu_label_item_widget.dart
+++ lib/layers/presentation/tales/widgets/menu_label_item_widget.dart
@@ -8,7 +8,9 @@ import '../screens/tale_detail_screen.dart';
 
 class MenuLabelItemWidget extends StatefulWidget {
   final MenuItem manuItem;
-  const MenuLabelItemWidget({super.key, required this.manuItem});
+  final bool isActive;
+  const MenuLabelItemWidget(
+      {super.key, required this.manuItem, required this.isActive});
 
   @override
   State<MenuLabelItemWidget> createState() => _MenuLabelItemWidgetState();
@@ -17,42 +19,32 @@ class MenuLabelItemWidget extends StatefulWidget {
 class _MenuLabelItemWidgetState extends State<MenuLabelItemWidget> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      // width: MediaQuery.of(context).size.width * 0.25,
-      child: Padding(
-        padding: EdgeInsets.only(
-          left: UIConstants.screenPadding,
-          right: menuItems.indexOf(widget.manuItem) == menuItems.length - 1
-              ? UIConstants.screenPadding
-              : 0,
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.horizontal(
-            left: Radius.elliptical(50, 50),
-            right: Radius.elliptical(50, 50),
-          ),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: UIConstants.minPadding),
-              // height: double.maxFinite,
-              decoration: BoxDecoration(
-                border: Border.all(color: AppColors.white, width: 1.0),
-                borderRadius: BorderRadius.horizontal(
-                  left: Radius.elliptical(50, 50),
-                  right: Radius.elliptical(50, 50),
-                ),
-              ),
-              child: Center(
-                  child: Text(
-                widget.manuItem.name,
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      fontFamily: "UberRegular",
-                      color: AppColors.white,
-                    ),
-              )),
+    return ClipRRect(
+      borderRadius: BorderRadius.horizontal(
+        left: Radius.elliptical(50, 50),
+        right: Radius.elliptical(50, 50),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: UIConstants.minPadding),
+          // height: double.maxFinite,
+          decoration: BoxDecoration(
+            color: widget.isActive ? AppColors.white : Colors.transparent,
+            border: Border.all(color: AppColors.white, width: 1.0),
+            borderRadius: BorderRadius.horizontal(
+              left: Radius.elliptical(50, 50),
+              right: Radius.elliptical(50, 50),
             ),
           ),
+          child: Center(
+              child: Text(
+            widget.manuItem.name,
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  fontFamily: "UberMedium",
+                  color: widget.isActive ? AppColors.black : AppColors.white,
+                ),
+          )),
         ),
       ),
     );
---
+++ lib/layers/presentation/tales/widgets/tales/stories_card_tab_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StoriesCardTabWidget extends StatefulWidget {
+  const StoriesCardTabWidget({super.key});
+
+  @override
+  State<StoriesCardTabWidget> createState() => _StoriesCardTabWidgetState();
+}
+
+class _StoriesCardTabWidgetState extends State<StoriesCardTabWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Story Tab Content");
+  }
+}
